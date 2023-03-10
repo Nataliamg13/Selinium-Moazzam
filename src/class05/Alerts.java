8dae8bfa26b8434d0e6621285841c18fa9eda366
Nataliamg13 <nataliamg13@gmail.com>
2023-03-10 17:00:51 -0600

    class05

---
+++ src/class05/Alerts.java
@@ -0,0 +1,35 @@
+package class05;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alerts {
+    public static void main(String[] args) throws InterruptedException {
+
+//        tell your project where the webdriver is located.
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        //  create an instance of WebDriver
+        WebDriver driver = new ChromeDriver();
+        //        max the window
+        driver.manage().window().maximize();
+
+       //        goto syntaxprojects.com
+        driver.get("http://practice.syntaxtechs.net/basic-select-dropdown-demo.php");
+
+        //click on the button for simple prompt alert
+        driver.findElement(By.xpath("//button[@onclick='myAlertFunction()']")).click();
+        //use switchTD
+       Alert alert = driver.switchTo().alert();
+       Thread.sleep(2000);
+       alert.accept();
+
+       driver.findElement(By.xpath("//button[@onclick='myPromptFunction()']")).click();
+
+      alert.sendKeys("abracadabra");
+
+      alert.accept();
+
+    }
+}
---
+++ src/class05/MultiDropDown.java
@@ -0,0 +1,42 @@
+package class05;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class MultiDropDown {
+    public static void main(String[] args) throws InterruptedException {
+
+//        tell your project where the webdriver is located.
+
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+
+        //        create an instance of WebDriver
+        WebDriver driver = new ChromeDriver();
+//        max the window
+        driver.manage().window().maximize();
+
+//        goto syntaxprojects.com
+        driver.get("http://practice.syntaxtechs.net/basic-select-dropdown-demo.php");
+
+//        get the dropDown
+        WebElement DD = driver.findElement(By.xpath("//select[@id='multi-select']"));
+//        use the select class
+        Select sel = new Select(DD);
+
+//        Select by value
+        sel.selectByValue("New Jersey");
+//        select by visible text
+        sel.selectByVisibleText("Ohio");
+//              too slow
+        Thread.sleep(3000);
+//        When it comes MultiSelect Drop Down we can also deselect
+        sel.deselectByValue("New Jersey");
+
+        sel.deselectByVisibleText("Ohio");
+        //System.out.println();
+
+
+    }
+}
